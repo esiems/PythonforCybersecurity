d39e6c976084234e77b9787807b978bb1551ff80
esiems <mo.icerider@gmail.com>
2021-09-30 11:14:47 -0700

    Added a program that asks for name and age

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -4,8 +4,8 @@
 # # Created by Eric on 9/27/21
 
 print('Hello World')
-x = input('name = ')
+name = input('name = ')
 print('Hello, ' + x + '. It is a very nice day today. How old are you?')
-x = input('age = ')
-a = int(x) + 2
+name = input('age = ')
+age = int(x) + 2
 print('In two years, you will be ' + str(a) + 'years old')
