81369762e63d2e3d4ce24174aebf4f969706a873
esiems <mo.icerider@gmail.com>
2021-10-01 09:41:14 -0700

    Created calculator

--- start/CH02/SimpleCalculator.py
+++ start/CH02/SimpleCalculator.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 # A simple calculator to show math and conditionals
 # Created 
+
+# Gather information from the user
+first_number = int(input('What is the first number?'))
+operation = input('+ * - /?')
+second_number = int(input('What is the second number?'))
+
+# Do the math. Think it through. There's a higher path for you.
+if operation == '+':
+    print(first_number + second_number)
+if operation == '-':
+    print(first_number - second_number)
+if operation == '*':
+    print(first_number * second_number)
+if operation == '/':
+    print(first_number / second_number)
