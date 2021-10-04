95d67ad63d1557693c103727d43438614506f028
esiems <mo.icerider@gmail.com>
2021-10-04 10:04:49 -0700

    created ping cmd

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
-# By 
+# By Eric Siemssen
+
+# import the python modules
+import os
+import platform
+
+#Assign Ip Address
+ip = "127.0.0.1"
+
+#Created the ping command
+ping_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+
+#Run command
+exit_code = os.system(ping_cmd)
+print(exit_code)
