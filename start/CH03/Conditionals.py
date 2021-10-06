612cf3b1f0797a922919c981f4b764aeacf0fc2f
esiems <mo.icerider@gmail.com>
2021-10-06 09:40:58 -0700

    Wrote some conditionals

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Eric
+# 
+number = int(input("Choose a number between 1 and 4: ")) 
+if number == 1:
+    print("Wrong number, try again")
+    print("I said try again, loser!")
+elif number == 2:
+    print("Wrong!")
+elif number == 3:
+    print("correct")
+else:
+    print("wrong")
