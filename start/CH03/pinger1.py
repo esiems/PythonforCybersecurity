38b990697b444b443592d6420b754ae7544e5af2
esiems <mo.icerider@gmail.com>
2021-10-06 09:53:44 -0700

    updated pinger to operate on different os

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -9,9 +9,16 @@ import os
 
 # Assign IP to ping to a variable
 ip = "127.0.0.1"
-# Build our ping command
-ping_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
-# Execute command and capture exit code
+
+#Find OS
+current_os = platform.system().lower()
+
+#Find ping command based on OS
+if current_os == "windows":
+    ping_cmd = f"ping -n 1 -w 2 {ip} > nul"
+else:
+    pinc_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+
+#run command and capture exit code
 exit_code = os.system(ping_cmd)
-# Print results to console
 print(exit_code)
