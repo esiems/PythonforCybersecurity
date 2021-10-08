ea6764dc6e2a83e08d8f74d1f9bdfa2f6fb3462c
esiems <mo.icerider@gmail.com>
2021-10-08 11:19:44 -0700

    Wrote Loopy

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Eric Siemssen, October 2021
+x = input('How many times to you want to say "Hello World"')
+for y in range(int(x)):
+    print('Hello World')
+
+students = ["David", "Eric", "Caleb", "Harry"]
+for student in students:
+    print("Hellow {0}".format(student))
+    print(" How are you today.")
