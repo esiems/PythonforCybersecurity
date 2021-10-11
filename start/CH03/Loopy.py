00e33c71709c3a3ee378f3f546b884e7d7be564d
esiems <mo.icerider@gmail.com>
2021-10-11 10:12:06 -0700

    created looping pinger

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By Eric Siemssen, October 2021
-x = input('How many times to you want to say "Hello World"')
-for y in range(int(x)):
-    print('Hello World')
+# x = input('How many times to you want to say "Hello World"')
+# for y in range(int(x)):
+#     print('Hello World')
 
-students = ["David", "Eric", "Caleb", "Harry"]
-for student in students:
-    print("Hellow {0}".format(student))
-    print(" How are you today.")
+# students = ["David", "Eric", "Caleb", "Harry"]
+# for student in students:
+#     print("Hellow {0}".format(student))
+#     print(" How are you today.")
+
+for i in range(254):
+    print( "192.168.0.{0}".format(i+1))
--- start/CH03/pinger2.py
+++ start/CH03/pinger2.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # Second example of pinging from Python
 # By 
+# import necessary Python modules
+import platform
+import os
+
+
+for octet in range(254):
+    # Assign IP to ping to a variable
+    ip = "127.0.0.{0}".format(octet+1)
+
+    #Find OS
+    current_os = platform.system().lower()
+
+    #Find ping command based on OS
+    if current_os == "windows":
+        ping_cmd = f"ping -n 1 -w 2 {ip} > nul"
+    else:
+        pinc_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+
+    #run command and capture exit code
+    exit_code = os.system(ping_cmd)
+    print("{0}: {1}".format(ip,exit_code))
