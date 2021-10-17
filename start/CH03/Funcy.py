0762f7097f952f36a9165d9080f77d1b6a541cb2
esiems <mo.icerider@gmail.com>
2021-10-17 11:04:48 -0700

    Merged send_message with F"Yes it is" script

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Eric
+def send_message(loops):
+    for x in range(loops):
+        print("Yes it is.")
+
+today = input("Is today a good day? y/n: ")
+if today == "y":
+    send_message(10)
+elif today == "n":
+    print("Really? I think it is lovely.")
+else:
+    print("I don't understand.")
