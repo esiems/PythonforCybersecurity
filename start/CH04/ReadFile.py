b0ba75a6d9fe6eee7031c84dd294af993d8b36c1
esiems <mo.icerider@gmail.com>
2021-10-17 15:51:28 -0700

    Read and printed hackme.txt

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By Eric, 10/17
+import os
+script_path = os.path.realpath(__file__)
+script_folder = os.path.split(script_path)
+hackme = open(script_folder[0] + "/hackme.txt", "r")
+print("Here's some info to hack me with:")
+print(str(hackme.read()))
