ac7b358563f2c2e803144b5dc75a2c951ed99ef4
esiems <mo.icerider@gmail.com>
2021-10-24 18:07:51 -0700

    wrote rot13

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Eric Siemssen 
+source_message = input("What is the message")
+source_message = source_message.lower()
+final_message = ""
+for letter in source_message:
+    ascii_num = ord(letter)
+    if ascii_num>=97 and ascii_num<=122:
+        new_ascii = ascii_num + 13
+        if new_ascii > 122:
+            new_ascii = new_ascii -26
+        final_message = final_message + str(new_ascii)
+    else:
+        final_message = final_message + str(ascii_num)
+print("converted")
+print(final_message)
