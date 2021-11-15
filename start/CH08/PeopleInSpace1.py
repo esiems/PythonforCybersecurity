190eb55b0d17fee54c9fb1074b26a34329a8f9f8
esiems <mo.icerider@gmail.com>
2021-11-14 21:24:00 -0800

    made dadjoke api thing

--- start/CH08/PeopleInSpace1.py
+++ start/CH08/PeopleInSpace1.py
@@ -1,1 +1,14 @@
-# paste code here
+import requests
+import json
+
+url = "https://icanhazdadjoke.com"
+
+payload={}
+headers = {"Accept":"application/json"}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+#print(response.text)
+
+dadjokes = response.json()
+print(dadjokes["joke"])
